19c477c1fb5a09a63992f6cc4c22252b46fc479b
yuanconghao <yuanconghao@gmail.com>
2023-05-04 14:17:42 +0800

    qa youtube

--- qa_youtube.py
+++ qa_youtube.py
@@ -12,7 +12,7 @@ from langchain.prompts.chat import (
 
 # 加载 youtube 频道
 loader = YoutubeLoader.from_youtube_url("https://www.youtube.com/watch?v=oU7-KMGZ4bc")
-#loader.language = "zh-Hans"
+# loader.language = "zh-Hans"
 # 将数据转成 document
 documents = loader.load()
 
@@ -58,9 +58,8 @@ chat_history = []
 
 while True:
     question = input('问题：')
-    print(question)
     # 开始发送问题 chat_history 为必须参数,用于存储对话历史
     result = qa({'question': question, 'chat_history': chat_history})
-    chat_history.append((question, result['answer']))
-    print(chat_history)
+    history = {'question': question, 'answer': result['answer']}
+    chat_history.append(history)
     print(result['answer'])
