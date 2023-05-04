330af20588491a4883c3a83368834dbbb1ecb2ab
yuanconghao <yuanconghao@gmail.com>
2023-05-04 14:05:10 +0800

    add html analysis

--- html_analysis.py
+++ html_analysis.py
@@ -1,4 +1,7 @@
 from langchain.document_loaders import UnstructuredURLLoader
+from langchain.chains.summarize import load_summarize_chain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain import OpenAI
 
 urls = [
     "https://www.understandingwar.org/backgrounder/russian-offensive-campaign-assessment-february-8-2023",
@@ -10,3 +13,22 @@ loader = UnstructuredURLLoader(urls=urls)
 data = loader.load()
 
 print(data)
+
+# 初始化文本分割器
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=500,
+    chunk_overlap=0
+)
+
+# 切分文本
+split_documents = text_splitter.split_documents(data)
+print(f'documents:{len(split_documents)}')
+
+# 加载llm模型
+llm = OpenAI(model_name="text-davinci-003", max_tokens=1500)
+
+# 创建总结链
+chain = load_summarize_chain(llm, chain_type="refine", verbose=True)
+
+# 执行总结链
+chain.run(split_documents[:5])
