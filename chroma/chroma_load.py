746cfdbdfae67bf5d46926bbb92c5e40b4da5834
yuanconghao <yuanconghao@gmail.com>
2023-05-05 16:29:57 +0800

    add model

--- chroma/chroma_load.py
+++ chroma/chroma_load.py
@@ -12,7 +12,7 @@ docsearch = Chroma(persist_directory="/data/vector_store", embedding_function=em
 time1 = time.time()
 qa = VectorDBQA.from_chain_type(llm=OpenAI(model_name='ada'), chain_type="stuff", vectorstore=docsearch,
                                 return_source_documents=True)
-# 进行问答
+# ada进行问答
 result = qa({"query": "秦逸是谁"})
 print(result)
 result = qa({"query": "51talk业务有哪些"})
@@ -24,7 +24,7 @@ print("model ada cost:", time2 - time1)
 time3 = time.time()
 qa = VectorDBQA.from_chain_type(llm=OpenAI(), chain_type="stuff", vectorstore=docsearch,
                                 return_source_documents=True)
-# 进行问答
+# text-davinci-003进行问答
 result = qa({"query": "秦逸是谁"})
 print(result)
 result = qa({"query": "51talk业务有哪些"})
